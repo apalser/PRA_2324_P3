58a0b921140495c782804ff1d5c7805a946a46c6
Angel Palacios Server <apalser@etsinf.upv.es>
2024-12-11 20:24:03 +0100

    Añadida interfaz genérica Dict

--- Makefile
+++ Makefile
@@ -7,6 +7,8 @@ bin/testHashTable: Dict.h HashTable.h TableEntry.h testHashTable.cpp
 bin/testBSTree: BSTree.h BSNode.h testBSTree.cpp
 	mkdir -p bin
 	g++ -o bin/testBSTree testBSTree.cpp BSTree.h BSNode.h
-
+bin/testBSTreeDict: BSTree.h BSNode.h TableEntry.h Dict.h testBSTreeDict.cpp
+	mkdir -p bin
+	g++ -o bin/testBSTreeDict testBSTreeDict.cpp BSTree.h BSNode.h BSTreeDict.h
 clean:
 	rm -rf *.o *.gch bin
--- TableEntry.h
+++ TableEntry.h
@@ -7,52 +7,34 @@
 template <typename V> 
 class TableEntry {
 	    public:
-		    std::string key;
-		    V value;
-		    TableEntry(std::string key, V value) {
-			    this->key = key;
-			    this->value = value;
-		    }
+		            std::string key;
+			    	V value;
+					TableEntry(std::string key, V value) {
+						        	this->key = key;
+								        	this->value = value;
+											}
+						TableEntry(std::string key) {
+							        	this->key = key;
+										}
+							TableEntry() {
+								        	this->key = "";
+											}
+								friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+											return (te1.key == te2.key)? true : false;
+												}
+									friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+												return (te1.key != te2.key)? true : false;
+													}
+										friend bool operator<(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+													return (te1.key < te2.key)? true : false;
+														}
+											friend bool operator>(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+														return (te1.key > te2.key)? true : false;
+															}
 
-		    TableEntry(std::string key) {
-			    this->key = key;
-		    }
-
-		    TableEntry() {
-			    this->key = "";
-		    }
-
-		    friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2) {
-			    if(te1.key == te2.key){
-				    return true;
-			    }else{
-				    return false;
-			    }
-		    }
-
-		    friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2) {
-			    if(te1.key == te2.key){
-				    return false;
-			    }else{
-				    return true;
-			    }
-		    }
-		    friend bool operator<(const TableEntry<V> &te1, const TableEntry<V> &te2) {
-			    if(te1.key > te2.key){
-				    return false;
-			    }else{
-				    return true;
-		    }
-		    friend bool operator>(const TableEntry<V> &te1, const TableEntry<V> &te2) {
-			   if(te1.key > te2.key){
-				   return true;
-			   }else{
-				   return false;
-		    }
-		    friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te) {
-			   out << "('" << te.key << "' => " << te.value << ")";
-			   return out;
-		    }
-
+												friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te) {
+															out << "('" << te.key << "' => " << te.value << ")";
+																	return out;
+																		}
 };
 #endif
